245011a81f5b6a7d500f281cb84b1c5f2583b49f
emelytecum <garcie16@tcnj.edu>
2024-11-25 10:18:25 -0500

    Add files via upload

---
+++ project.R
@@ -0,0 +1,13 @@
+# example
+
+# set working directory
+setwd("~/Desktop/sta215")
+
+# install "haven" packag 
+install.packages("haven")
+
+# load "haven" package
+library("haven")
+
+# load raw data
+raw_data <- read.csv("raw_data.csv")
